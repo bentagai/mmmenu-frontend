f96bbd41609bb771648faa4c178cc3fb9be19ccf
bentagai <bentagai@protonmail.com>
2020-04-30 20:17:21 +0100

    Mejoras Menu

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="card">
-    <v-carousel hide-delimiters>
-      <v-carousel-item
-        v-for="(item,i) in items"
-        :key="i"
-        :src="item.src"
-      ></v-carousel-item>
-    </v-carousel>
-    <h1 class="display-1 font-regular mb-2 mt-10">{{article.title}}</h1>
-    <h2 class="headline font-weight-regular mb-2">{{article.subtitle}}</h2>
-    <h2 class="subtitle-2 font-weight-regular mb-10">{{formatDate}}</h2>
-    <p class="title font-weight-regular" v-html="article.text"></p>
+  <div>
+    <v-container>
+      <v-carousel hide-delimiters>
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+      <h1 class="display-1 font-regular mb-2 mt-10">{{article.title}}</h1>
+      <h2 class="headline font-weight-regular mb-2">{{article.subtitle}}</h2>
+      <h2 class="subtitle-2 font-weight-regular mb-10">{{formatDate}}</h2>
+      <span class="title font-weight-regular" v-html="article.text"></span>
+    </v-container>
   </div>
 </template>
 
