4fa713d1ffdfc7257c715c7dbb94b3b929e73477
bentagai <bentagai@protonmail.com>
2020-05-06 16:45:41 +0100

    remove stuff

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,8 +3,7 @@ const API = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Content-Type': 'application/json'
   }
 })
 export default {
